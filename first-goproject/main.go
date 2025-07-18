69edc58a67bcb84afa69a451046d237690e097db
Fariha Promi <farihapromi2611@gmail.com>
2025-07-18 19:41:31 +0600

    Add multiple output

---
+++ first-goproject/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// single output return
+
+func add(num1 int, num2 int) int {
+	sum := num1 + num2
+	return sum
+}
+
+// multiple output return
+func getNumbers(num1 int, num2 int) (int, int) {
+	sum := num1 + num2
+	mul := num1 * num2
+	return sum, mul
+
+}
+
+//string
+func sayHello(name string) {
+	fmt.Println("Hello everyone,", name)
+}
+func main() {
+	// a := 10
+	// var a int = 10
+	// fmt.Println(a)
+	// a := 10
+	// b := 20
+	// sum := add(a, b)
+	// fmt.Println((sum))
+	// p, q := getNumbers(a, b)
+	// fmt.Println((p))
+	// fmt.Println((q))
+	sayHello("Promi")
+
+}
